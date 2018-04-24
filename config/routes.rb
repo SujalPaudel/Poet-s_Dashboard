fb9430b924ec391f2c4f4e6e75821c802cbeeaf1
SujalPaudel <thesujal17@gmail.com>
2018-04-24 23:36:27 +0545

    Customized the routes de' Portfolio

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
 
-  resources :portfolios
+  #provide the same routes but change in the show
+  resources :portfolios, except:[:show]
+  
+  get 'portfolio/:id', to: 'portfolios#show', as:'portfolio_show'
+
+  #it shows if its portfolio/:id, route to portfolios#show
   
   get 'about-me', to: 'pages#about'
   get 'contact-me', to: 'pages#contact'
