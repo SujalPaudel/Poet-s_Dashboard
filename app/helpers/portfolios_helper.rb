5deb4aeaa8b16a0131f3e0814627dc69c3b1eb8b
SujalPaudel <thesujal17@gmail.com>
2018-05-13 18:11:01 +0545

    Updated file upload, image upload function with custom click buttons

--- app/helpers/portfolios_helper.rb
+++ app/helpers/portfolios_helper.rb
@@ -1,2 +1,17 @@
 module PortfoliosHelper
+  def image_generator(height:, width:) #this is the helper method
+    "http://via.placeholder.com/#{height}x#{width}"
+  end
+
+  def portfolio_image img, type
+    if img
+      img
+    elsif type == 'thumb'
+      image_generator(height: '350', width: '200')
+    else
+      image_generator(height: '600', width: '400')
+    end
+  end    
 end
+
+
