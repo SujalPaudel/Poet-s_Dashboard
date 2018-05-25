89f1c2c6afbf42674f4b46a611f05da6a0c11665
SujalPaudel <thesujal17@gmail.com>
2018-05-25 08:35:55 +0545

    Updated production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,8 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.logger = Logger.new(STDOUT)
+  config.logger.level = Logger::DEBUG
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
